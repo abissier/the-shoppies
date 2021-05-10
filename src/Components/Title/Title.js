d3864a5702ff504c8051c2d2c71a94e5700c06f4
abissier <71886314+abissier@users.noreply.github.com>
2021-05-10 00:20:00 -0700

    added media query

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -2,7 +2,7 @@ function Title() {
 	return (
 		<main>
 			<h1>- The Shoppies -</h1>
-			<p>2021</p>
+			<p id="year">2021</p>
 			<p className="about-card">Search for your favorite films and add them to your top 5</p>
 		</main>
 	);
