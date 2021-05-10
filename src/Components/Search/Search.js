608752ed5bad4a53cd2fad2b225229783b3acaa2
abissier <71886314+abissier@users.noreply.github.com>
2021-05-09 18:53:30 -0700

    changes to search page

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -6,46 +6,65 @@ function Search() {
 	const [ query, setQuery ] = useState('');
 	const [ result, setResult ] = useState([]);
 
+	// grab valid query from form and send to search movies function
 	const handleFormSubmit = (event) => {
 		event.preventDefault();
-		searchMovies(query);
+		if (query === '' || query.trim() === '') {
+			alert('Please enter a valid movie title');
+		} else {
+			searchMovies(query);
+		}
 	};
 
+	// send query to API
 	const searchMovies = (query) => {
 		API.search(query).then((res) => setResult([ ...result, res.data ])).catch((err) => console.log(err));
 	};
 
+	// delete item from results
+	const handleDelete = (event) => {
+		let movieName = event.target.attributes[3].value;
+		let newResults = result.filter((item) => item.Title !== movieName);
+		setResult([ ...newResults ]);
+	};
+
 	return (
-		<div>
-			<h1>Search by movie title</h1>
-			<input
-				type="text"
-				onChange={(e) => {
-					setQuery(e.target.value);
-				}}
-				placeholder="search by movie name"
-			/>
-			<button type="submit" onClick={handleFormSubmit}>
-				Search
-			</button>
+		<main>
+			<h1>- Search by movie title -</h1>
+			<form>
+				<input
+					type="text"
+					onChange={(e) => {
+						setQuery(e.target.value);
+					}}
+					placeholder="Enter movie name"
+				/>
+				<button type="submit" className="search-btn" onClick={handleFormSubmit}>
+					<img src="https://img.icons8.com/pastel-glyph/64/000000/clapperboard--v2.png" alt="clapboard" />
+				</button>
+			</form>
 			<section className="movie-card-container">
-				{result.map((item) => {
-					return (
-						<Card
-							key={item.Title}
-							poster={item.Poster}
-							title={item.Title}
-							releaseDate={item.Released}
-							director={item.Director}
-							actors={item.Actors}
-							plot={item.Plot}
-							result={result}
-							setResult={setResult}
-						/>
-					);
-				})}
+				{result ? (
+					result.map((item) => {
+						return (
+							<Card
+								key={item.Title}
+								poster={item.Poster}
+								title={item.Title}
+								releaseDate={item.Released}
+								director={item.Director}
+								actors={item.Actors}
+								plot={item.Plot}
+								result={result}
+								handleDelete={handleDelete}
+							/>
+						);
+					})
+				) : (
+					0
+				)}
 			</section>
-		</div>
+		</main>
 	);
 }
 
