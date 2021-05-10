8e45293a5f795644c7be0fb5ec081620cf7a4ad6
abissier <71886314+abissier@users.noreply.github.com>
2021-05-09 19:50:33 -0700

    movie card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,19 +1,16 @@
 import { useState } from 'react';
 
-function Card({ poster, title, plot, director, actors, releaseDate, result, setResult }) {
+function Card({ poster, title, director, releaseDate, result, handleDelete, handleDeleteSaved }) {
 	const [ results, setResults ] = useState(result);
 
-	// delete item from results
-	const handleDelete = (event) => {
-		let movieName = event.target.value;
-		let newResults = results.filter((item) => item.Title !== movieName);
-		setResult([ ...newResults ]);
-	};
-
-	// set favorite movies to local storage if not already there
-	const handleSave = (event) => {
-		let movieTitle = event.target.value;
-		if (!localStorage.getItem(movieTitle)) {
+	// set favorite movies to local storage if not already there and has not met top 5
+	function handleSave(event) {
+		let movieTitle = event.target.attributes[3].value;
+		if (localStorage.getItem(movieTitle)) {
+			alert('This movie has already been saved');
+		} else if (localStorage.length >= 5) {
+			alert("You've already saved 5 movies");
+		} else {
 			let movieObject = results.filter((result) => result.Title === movieTitle);
 			let storeMovie = {
 				poster: movieObject[0].Poster,
@@ -24,28 +21,42 @@ function Card({ poster, title, plot, director, actors, releaseDate, result, setR
 				releaseDate: movieObject[0].Released
 			};
 			localStorage.setItem(movieTitle, JSON.stringify(storeMovie));
-		} else {
-			console.log('already saved');
 		}
-	};
+	}
 
 	return (
 		<article className="movie-card">
-			<div className="movie-card-btns">
-				<button value={title} onClick={handleDelete}>
-					X
-				</button>
-				<button value={title} onClick={handleSave}>
-					Heart
-				</button>
-			</div>
 			<div className="movie-info">
 				<img src={poster} alt={title} />
 				<h1>{title}</h1>
-				<p>{releaseDate}</p>
-				<p>{director}</p>
-				<p>{actors}</p>
-				<p>{plot}</p>
+				<p>Released {releaseDate}</p>
+				<p>Directed by: {director}</p>
+			</div>
+			<div className="movie-card-btns">
+				<div className="tooltip">
+					<button>
+						<img
+							src="https://img.icons8.com/dotty/80/000000/delete-ticket.png"
+							alt="delete movie ticket"
+							height="40px"
+							value={title}
+							onClick={handleDelete || handleDeleteSaved}
+						/>
+					</button>
+					<span className="tooltiptext">Remove Film</span>
+				</div>
+				<div className="tooltip">
+					<button className="save-btn">
+						<img
+							src="https://img.icons8.com/dotty/80/000000/add-ticket.png"
+							alt="save movie ticket"
+							height="40px"
+							value={title}
+							onClick={handleSave}
+						/>{' '}
+					</button>
+					<span className="tooltiptext">Save Film</span>
+				</div>
 			</div>
 		</article>
 	);
