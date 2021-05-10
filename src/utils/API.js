733149ef03de165fec75331060a328ff052b30d6
abissier <71886314+abissier@users.noreply.github.com>
2021-05-09 19:49:32 -0700

    api to omdb

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const BASEURL = 'https://www.omdbapi.com/?t=';
-const APIKEY = '&apikey=f4ee69aa';
+const APIKEY = '&type=movie&apikey=f4ee69aa';
 
 export default {
 	search: function(query) {
