21b747b006551b9627879733cb8241879f4f23d8
abissier <71886314+abissier@users.noreply.github.com>
2021-05-09 19:51:27 -0700

    title component

---
+++ src/Components/Title/Title.js
@@ -0,0 +1,11 @@
+function Title() {
+	return (
+		<main>
+			<h1>- The Shoppies -</h1>
+			<p>2021</p>
+			<p className="about-card">Search for your favorite films and add them to your top 5</p>
+		</main>
+	);
+}
+
+export default Title;
